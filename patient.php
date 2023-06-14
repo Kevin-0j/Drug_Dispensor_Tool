566dcf567d9b459628627dc50646638dfbf0c37a
Thuo's Space <131641951+mmwithiga@users.noreply.github.com>
2023-06-14 23:49:22 +0300

    Update and rename patients.php to patient.php

---
+++ patient.php
@@ -0,0 +1,23 @@
+<?php
+require 'connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $firstName = $_POST['first_name'];
+    $lastName = $_POST['last_name'];
+    $ssn = $_POST['ssn'];
+    $address = $_POST['address'];
+    $medicalHistory = $_POST['medical_history'];
+
+    $sql = "INSERT INTO patient (FName, LName, SSN, P_Address, MedicalHistory)
+            VALUES ('$firstName', '$lastName', '$ssn', '$address', '$medicalHistory')";
+
+    echo "<br>";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+}
+?>
