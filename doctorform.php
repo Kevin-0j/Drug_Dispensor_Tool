1cebfb5348b40ff2ed4a576430685fa9dddc2d3a
Thuo's Space <131641951+mmwithiga@users.noreply.github.com>
2023-06-14 23:46:02 +0300

    Updated doctorform.php

--- doctorform.php
+++ doctorform.php
@@ -7,19 +7,23 @@
   <h2>Doctor Form</h2>
   <form action="doctor.php" method="post">
     <label for="ssn">SSN:</label>
-    <input type="text" id="ssn" name="ssn" required><br><br>
+    <input type="text" id="SSN" name="SSN" required><br><br>
     
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" required><br><br>
+    <label for="name">FName:</label>
+    <input type="text" id="FName" name="FName" required><br><br>
     
-    <label for="specialty">Specialty:</label>
-    <input type="text" id="specialty" name="specialty" required><br><br>
+    <label for="name">LName:</label>
+    <input type="text" id="LName" name="LName" required><br><br>
+
+    <label for="Speciality">Speciality:</label>
+    <input type="text" id="Speciality" name="Speciality" required><br><br>
+
     
     <label for="experience">Years of Experience:</label>
-    <input type="number" id="experience" name="experience" required><br><br>
+    <input type="number" id="Experience" name="Experience" required><br><br>
     
     <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required><br><br>
+    <input type="email" id="Email" name="Email" required><br><br>
     
     <input type="submit" value="Submit">
   </form>
